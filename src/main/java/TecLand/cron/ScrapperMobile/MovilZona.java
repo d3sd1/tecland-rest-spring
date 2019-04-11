46e580f91f1b67cc37a60f121b4ad1c4de0069e7
jorge <jorge.rios.marfil@gmail.com>
2019-04-11 23:05:52 +0200

    I've added ScrapperMobile package with the first class I'm going to use

---
+++ src/main/java/TecLand/cron/ScrapperMobile/MovilZona.java
@@ -0,0 +1,14 @@
+package TecLand.cron.ScrapperMobile;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovilZona {
+
+    @Profile({"dev","test"})
+    @Scheduled(fixedDelay = 5000)//Every 5000ms or 5s
+    public void ScrapperMovilZona(){
+    }
+}

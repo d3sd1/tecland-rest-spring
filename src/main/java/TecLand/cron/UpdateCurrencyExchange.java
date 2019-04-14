d737eff63c230124cd32164eb24e64e95ede44b0
root <1234>
2019-04-14 19:57:04 +0200

    No sé si he entendido bien el problema, pero lo aquí está resuelto

--- src/main/java/TecLand/cron/UpdateCurrencyExchange.java
+++ src/main/java/TecLand/cron/UpdateCurrencyExchange.java
@@ -1,6 +1,5 @@
 package TecLand.cron;
 
-import TecLand.Logger.LogService;
 import TecLand.ORM.Model.CurrencyValue;
 import TecLand.ORM.Repository.CurrencyRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -20,12 +19,10 @@ public class UpdateCurrencyExchange {
     @Autowired
     private CurrencyRepository currencyRepository;
 
-    @Autowired
-    private LogService logger;
 
     @Scheduled(fixedRate = 30000) // Every 30S (30.000 ms), w/ beggining
     public void updateCurrencies() {
-        logger.info("Updating currencies...");
+        System.out.println("Updating currencies...");
 
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -35,8 +32,10 @@ public class UpdateCurrencyExchange {
         ResponseEntity<String> responseEntity = rest.exchange("https://api.exchangeratesapi.io/latest?base=EUR", HttpMethod.GET, requestEntity, String.class);
         HttpStatus status = responseEntity.getStatusCode();
         if (status.is2xxSuccessful()) {
-            logger.info("Currency rate exchange updated.");
+            System.out.println("Currency rate exchange updated.");
+
             ObjectMapper mapper = new ObjectMapper();
+
             Map<String, Object> obj = new HashMap<String, Object>();
 
             try {
@@ -56,12 +55,14 @@ public class UpdateCurrencyExchange {
                 }
 
             } catch (Exception e) {
-                logger.warning("Currency update error: " + e.getMessage());
+                System.out.println("Currency update error: ");
+                e.printStackTrace();
             }
         } else {
-            logger.warning("Currency rate exchange update error: " + status);
+            System.out.println("Currency rate exchange update error: " + status);
         }
     }
+
 
     private CurrencyValue updateCurrencyDb(String baseRateName, double baseRateVal, CurrencyValue baseCurrency) {
         CurrencyValue baseCurencyDb = this.currencyRepository.findByKeyName(baseRateName);
--- src/main/java/TecLand/utils/Currencies.java
+++ src/main/java/TecLand/utils/Currencies.java
@@ -1,9 +1,10 @@
 package TecLand.utils;
 
 import TecLand.ORM.Model.CurrencyValue;
+import TecLand.ORM.Repository.CurrencyRepository;
 
 public class Currencies {
-    public void convert(CurrencyValue currency1, CurrencyValue currency2) {
-
+    public float convert(CurrencyValue currency1, CurrencyValue currency2) {
+       return currency1.getCurrencyExchangeValue()*1/currency2.getCurrencyExchangeValue();
     }
 }

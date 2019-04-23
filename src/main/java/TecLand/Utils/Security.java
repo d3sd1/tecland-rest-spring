f8ff1f03a78661414abaa9096d358419efa14c99
Andrei García (Sobremesa) <andreigarciacuadra@gmail.com>
2019-04-23 08:10:27 +0200

    jwt expiration fix

--- src/main/java/TecLand/Utils/Security.java
+++ src/main/java/TecLand/Utils/Security.java
@@ -4,18 +4,15 @@ import TecLand.Logger.LogService;
 import TecLand.ORM.Model.DashUserLogin;
 import TecLand.ORM.Repository.DashUserLoginRepository;
 import com.lambdaworks.crypto.SCryptUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
-import java.util.Date;
 import java.security.MessageDigest;
+import java.util.Date;
 
 @Service("security")
 public class Security {
@@ -40,9 +37,18 @@ public class Security {
         if (hash == null) {
             hash = "emptyHashPreventNullPointer";
         }
-        return Jwts.parser()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(hash))
-                .parseClaimsJws(jwt).getBody();
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary(hash))
+                    .parseClaimsJws(jwt).getBody();
+        } catch (ExpiredJwtException e) {
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return claims;
     }
 
     public boolean isJWTExpired(String jwt, String hash) {
@@ -52,10 +58,10 @@ public class Security {
 
     public boolean validateSession(String jwt) {
         DashUserLogin dbUserLogin = dashUserLoginRepository.findByJwt(jwt);
-        if(null == dbUserLogin) {
+        if (null == dbUserLogin) {
             return false;
         }
-        if(this.isJWTExpired(dbUserLogin.getJwt(), dbUserLogin.getHash())) {
+        if (this.isJWTExpired(dbUserLogin.getJwt(), dbUserLogin.getHash())) {
             return false;
         }
         return true;

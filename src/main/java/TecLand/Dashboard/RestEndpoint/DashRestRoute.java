56808b08c5755797c3a0eda50914847ca1c2fefa
andreigarcia <andreigarciacuadra@gmail.com>
2019-04-24 18:27:09 +0200

    some major fixes

--- src/main/java/TecLand/Dashboard/RestEndpoint/DashRestRoute.java
+++ src/main/java/TecLand/Dashboard/RestEndpoint/DashRestRoute.java
@@ -9,7 +9,7 @@ public class DashRestRoute {
     private static final String PREFIX = "/dash";
 
     /* CONTROLLER ROUTES */
-    public static final String LOGIN = PREFIX + "/auth/login";
+    public static final String LOGIN = PREFIX + LOGIN_NOT_NEEDED_PREFIX + "/login";
     public static final String LOGOUT = PREFIX + "/logout";
     public static final String CHECK_SESSION = PREFIX + "/session";
     public static final String SESSION_DATA = PREFIX + "/session/data";
